fcebcb7a57322735773f79296ead7504383be853
JanekiaPinkard <janekia.pinkard@gmail.com>
2021-12-10 16:17:16 -0500

    my commit 4

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '291764286184072',
+        clientSecret: '0f7b863cf0db1b13806d19aba2eeaefa'
+    }
+}
